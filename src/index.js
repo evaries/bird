6cd60255093f915b10868f5928e7ba2a9e2eb492
Evgeniy Nadtochiy <evgeniyna@intern.blackwood.gg>
2021-03-10 18:16:00 +0200

    add gravity

--- src/index.js
+++ src/index.js
@@ -5,24 +5,38 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-  default: 'arcade'
+  default: 'arcade',
+  arcade: {
+    gravity: {y:200}
+  }
   },
   scene: {
     preload,
-    create
+    create,
+    update
   }
 }
 
+let bird = null; 
+let totalDelta = null;
+
 // loading assets
 function preload(){
-  // 'this' context -> scene with a lot of functions and props
+  // 'this' context -> scene. with a lot of functions and props
   this.load.image('sky', 'assets/sky.png')
+  this.load.image('bird', 'assets/bird.png')
 }
 
 // initialise instance
 function create(){
   this.add.image(0, 0, 'sky').setOrigin(0, 0)
+  bird = this.physics.add.image(config.width / 10, config.height / 2, 'bird').setOrigin(0)
+  bird.body.gravity.y = 200;
+}
 
+// update frames (60 fps)
+function update(time, delta){
+totalDelta += delta
 }
 
 new Phaser.Game(config)
